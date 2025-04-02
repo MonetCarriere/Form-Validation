afda65ccf16f0ca7d4801b3d8a645fc5244de937
MonetCarriere <MonetCarriere@yahoo.com>
2025-04-02 13:43:45 -0700

    first commit

---
+++ js/login.js
@@ -0,0 +1,29 @@
+class Login {
+    constructor(form, fields) {
+        this.form = form;
+        this.fields = fields;
+        this.validateonSubmit();
+    }
+
+    validateonSubmit() {
+        let self = this;
+
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            self.fields.forEach((field) => {
+                const input = document.querySelector(`#${field}`);
+                console.log(input.value);
+            });
+        });
+    }
+}
+
+const form = document.querySelector(".loginForm");
+if (form) {
+    const fields = ["username", "password"];
+    const validator = new Login(form, fields);
+}
+
+
+
+
